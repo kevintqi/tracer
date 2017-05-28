638655fa186a9976f7192cd5dbda5f2982f00e4d
Kevin Qi <kevin.t.qi@gmail.com>
2017-05-28 16:17:09 -0700

    Static index.html

---
+++ public/lib/task_list.js
@@ -0,0 +1,34 @@
+function taskRowWriter(rowIndex, record, columns, cellWriter) {
+    var row;
+    row = '<td class="thumbnail">' + '<img class="thumbnail" src="' + record.thumbnail + '"/>' + '</td>';
+    row += '<td class="address">' + record.address + '</td>';
+    row += '<td class="time">' + record.scheduledTime + '</td>';
+    row += '<td class="assignment">' + record.assignment + '</td>';
+    return '<tr>' + row + '</tr>';
+}
+
+$(document).ready(function() {
+    $.getJSON("task_list.json", function(taskData, status) {
+        if (status === "success") {
+            $('#list_table_id').dynatable({
+                features: {
+                    paginate: false,
+                    recordCount: false,
+                    sorting: true
+                },
+                inputs: {
+                    processingText: 'Loading <img src="images/loading.gif" />'
+                },
+                dataset: {
+                    records: taskData
+                },
+                writers: {
+                    _rowWriter: taskRowWriter
+                }
+            });
+        }
+        else {
+            alert(status);
+        }
+    });
+});
