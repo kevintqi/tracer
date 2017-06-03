f87568fcb595a5a04accfd3f78a2301dc14654a7
Kevin Qi <kevin.t.qi@gmail.com>
2017-06-03 14:33:17 -0700

    Use AngularJS for switching views

--- public/lib/task_list.js
+++ public/lib/task_list.js
@@ -1,10 +1,35 @@
+var taskApp = angular.module('taskApp', []);
+ 
+taskApp .config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/task_list', {
+        templateUrl: 'templates/list.html',
+        controller: 'ListController'
+      }).
+      when('/task_details', {
+        templateUrl: 'templates/details.html',
+        controller: 'DetailsController'
+      }).
+      otherwise({
+        redirectTo: '/task_list'
+      });
+}]);
+
+taskApp.controller('ListController', function($scope) {
+});
+
+taskApp.controller('DetailsController', function($scope) {
+});
+
 function taskRowWriter(rowIndex, record, columns, cellWriter) {
     var row;
-    row = '<td class="thumbnail">' + '<img class="thumbnail" src="' + record.thumbnail + '"/>' + '</td>';
+    row =  '<td class="thumbnail"> <a href="#task_details">' + 
+           '<img class="thumbnail" src="' + record.thumbnail + '"/>' + '</a></td>';
     row += '<td class="address">' + record.address + '</td>';
     row += '<td class="time">' + record.scheduledTime + '</td>';
     row += '<td class="assignment">' + record.assignment + '</td>';
-    return '<tr class="w3-border-bottom">' + row + '</tr>';
+    return '<tr class="w3-border-bottom">' +  row + '</tr>';
 }
 
 $(document).ready(function() {
